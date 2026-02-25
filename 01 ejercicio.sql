5750662ff90f9937b6373eb8e02c827a451ce28b
Andresb275 <habuesaquillo.0730@unicesmag.edu.co>
2026-02-25 11:40:24 -0500

    Agregue archivo solucion ejercicio 1

---
+++ 01 ejercicio.sql
@@ -0,0 +1,6 @@
+-- Ejercicio 1
+-- ¿Cuántos clientes tenemos registrados en total?
+-- Se usa COUNT(*) para contar todas las filas de la tabla clientes.
+
+SELECT COUNT(*) AS total_clientes
+FROM clientes;
